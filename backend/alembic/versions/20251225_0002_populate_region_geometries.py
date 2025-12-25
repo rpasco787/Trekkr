1b0b736d547d2ed760f5878d1a3e7b9a7554812d
Ryan Pascual <rpascual789@gmail.com>
2025-12-25 13:52:38 -0800

    migration: add skeleton for region geometry population

---
+++ backend/alembic/versions/20251225_0002_populate_region_geometries.py
@@ -0,0 +1,19 @@
+"""populate region geometries from natural earth"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "20251225_0002"
+down_revision = "20251224_0001"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
